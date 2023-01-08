efaa43527d9e2e1228fafa152cf1234d91f6966e
Tetreur <90622976+Tetreur@users.noreply.github.com>
2023-01-08 23:38:02 +0100

    feat(setup_with_homebrew.zsh): Temporary comment brew bundle

--- setup_with_homebrew.zsh
+++ setup_with_homebrew.zsh
@@ -4,6 +4,6 @@ printf "\n<<< Starting Homebrew setup >>>\n\n"
 
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew bundle --verbose
+# brew bundle --verbose
 
 printf "\n<<< Homebrew setup finished >>>\n\n"
