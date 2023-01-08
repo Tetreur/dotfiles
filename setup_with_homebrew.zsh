c28fc539224a50b7a2fbfbbf831303cfdef2953a
Tetreur <90622976+Tetreur@users.noreply.github.com>
2023-01-08 23:02:31 +0100

    feat(Brewfile): Create a brewfile then use it with brew bundle
    Since all packages will be installed with bundle, i updated "setup_with_bhomebrew.zsh" accordingly

--- setup_with_homebrew.zsh
+++ setup_with_homebrew.zsh
@@ -4,12 +4,6 @@ printf "\n<<< Starting Homebrew setup >>>\n\n"
 
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# brew install httpie
-# brew install bat
-
-# brew cask install --no-quarantine google-chrome
-# brew cask install --no-quarantine firefox
-# brew cask install --no-quarantine visual-studio-code
-# brew cask install --no-quarantine alfred
+brew bundle --verbose
 
 printf "\n<<< Homebrew setup finished >>>\n\n"
