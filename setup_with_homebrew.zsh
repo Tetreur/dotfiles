286958ecf55d4ceb6445fb9a45b491fcfae3bc97
Tetreur <90622976+Tetreur@users.noreply.github.com>
2023-01-08 22:32:53 +0100

    feat(setup_with_homebrew.zsh): Add a command in dotbot script to setup with a homebrew related file
    Add command to the "setup_with_homebrew.zsh" file

---
+++ setup_with_homebrew.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+printf "\n<<< Starting Homebrew setup >>>\n\n"
+
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# brew install httpie
+# brew install bat
+
+# brew cask install google-chrome
+# brew cask install firefox
+# brew cask install visual-studio-code
+
+printf "\n<<< Homebrew setup finished >>>\n\n"
